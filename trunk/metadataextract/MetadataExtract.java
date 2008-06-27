be760cd9c65cf9056246328cde2a7d9840d7110a
danielgalassi <danielgalassi@45e08da6-9c50-0410-9ea3-47aaf801ce1a>
2008-06-27 02:41:42 +0000

    First check-in.

---
+++ trunk/metadataextract/MetadataExtract.java
@@ -0,0 +1,13 @@
+package metadataextract;
+
+public class MetadataExtract {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/xmlutils/XMLUtils.java
@@ -0,0 +1,5 @@
+package xmlutils;
+
+public class XMLUtils {
+
+}
