38eca82fcd197a564200a28e0d84ea1c9ab440fc
danielgalassi@gmail.com <danielgalassi@gmail.com@45e08da6-9c50-0410-9ea3-47aaf801ce1a>
2014-04-11 03:26:42 +0000

    Up-to-date Javadoc.

--- trunk/src/obiee/udmlparser/bundledApps/package-info.java
+++ trunk/src/obiee/udmlparser/bundledApps/package-info.java
@@ -1,8 +1,6 @@
 /**
- * 
- */
-/**
- * @author danielgalassi
+ * Applications shipped with UDMLParser (such as BusMatrix generator)
+ * @author danielgalassi@gmail.com
  *
  */
 package obiee.udmlparser.bundledApps;
