aeddc7a4be1db31912b76d873b98587128a78de0
danielgalassi@gmail.com <danielgalassi@gmail.com@45e08da6-9c50-0410-9ea3-47aaf801ce1a>
2015-03-10 13:24:07 +0000

    Feature to invoke bus matrices with metrics added

--- branches/AddMeasuresToMatrix/src/obiee/udmlparser/cli/Request.java
+++ branches/AddMeasuresToMatrix/src/obiee/udmlparser/cli/Request.java
@@ -16,6 +16,7 @@ public class Request {
 	/** A set of (key,value) pairs representing all (no flags) attributes*/
 	private Map<String, String> request = new HashMap<String, String>();
 	private boolean isBusMatrixInvoked = false;
+	private boolean isFullMatrixInvoked = false;
 	private boolean isTransformationInvoked = false;
 
 	public void invokeTransformation(boolean invoked) {
@@ -28,10 +29,18 @@ public class Request {
 
 	public void invokeBusMatrix(String value) {
 		isBusMatrixInvoked = (value.equals("busmatrix"));
+		
+		if (!isBusMatrixInvoked) {
+			isFullMatrixInvoked = (value.equals("metrics"));
+		}
 	}
 
 	public boolean isBusMatrixInvoked() {
 		return isBusMatrixInvoked;
+	}
+
+	public boolean isFullMatrixInvoked() {
+		return isFullMatrixInvoked;
 	}
 
 	/**
